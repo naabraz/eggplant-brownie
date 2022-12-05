a4b333b47ce4307c1171743e5b023ec69fc7efd4
Natalia Braz <naabraz@gmail.com>
2022-12-05 09:40:28 -0300

    chore: add controller marks

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -6,26 +6,32 @@ protocol AdicionaRefeicaoDelegate {
 
 class ViewController: UIViewController {
   
-  var delegate: AdicionaRefeicaoDelegate?
-    
-  @IBOutlet var nomeTextField: UITextField?
-  @IBOutlet weak var felicidadeTextField: UITextField?
+    // MARK: - Atributos
   
-  @IBAction func adicionar(_ sender: Any) {
-    guard let nomeDaRefeicao = nomeTextField?.text else {
-      return
+    var delegate: AdicionaRefeicaoDelegate?
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var nomeTextField: UITextField?
+    @IBOutlet weak var felicidadeTextField: UITextField?
+  
+    // MARK: - IBActions
+    
+    @IBAction func adicionar(_ sender: Any) {
+        guard let nomeDaRefeicao = nomeTextField?.text else {
+            return
+        }
+        
+        guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
+            print("erro ao obter felicidade")
+            return
+        }
+        
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        
+        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
+        delegate?.add(refeicao)
+        navigationController?.popViewController(animated: true)
     }
-    
-    guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
-      print("erro ao obter felicidade")
-      return
-    }
-    
-    let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
-    
-    print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
-      
-    delegate?.add(refeicao)
-    navigationController?.popViewController(animated: true)
-  }
 }
