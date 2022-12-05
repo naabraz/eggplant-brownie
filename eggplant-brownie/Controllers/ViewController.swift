9089c3401fbb75cf94dc5ecf7f619ff76d2085a8
Natalia Braz <naabraz@gmail.com>
2022-12-05 10:03:33 -0300

    chore: add table view datasource

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -4,17 +4,35 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
   
     // MARK: - Atributos
   
     var delegate: AdicionaRefeicaoDelegate?
+    var itens: [String] = ["Molho de Tomate", "Queijo", "Molho Apimentado", "Manjericão"]
     
     // MARK: - IBOutlets
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet weak var felicidadeTextField: UITextField?
   
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        
+        celula.textLabel?.text = item
+        
+        return celula
+    }
+    
     // MARK: - IBActions
     
     @IBAction func adicionar(_ sender: Any) {
