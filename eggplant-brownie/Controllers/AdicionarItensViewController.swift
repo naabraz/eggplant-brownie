ae96968ddd6b08eba0f41a7f24878a4e514e7ae3
Natalia Braz <naabraz@gmail.com>
2022-12-07 09:55:20 -0300

    feat: add adicionar itens method

--- eggplant-brownie/Controllers/AdicionarItensViewController.swift
+++ eggplant-brownie/Controllers/AdicionarItensViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class AdicionarItensViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var caloriasTextField: UITextField!
+    
     // MARK: - View life cycle
     
     override func viewDidLoad() {
@@ -11,6 +16,16 @@ class AdicionarItensViewController: UIViewController {
     // MARK: - IBAction
     
     @IBAction func adicionarItem(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
+        
+        guard let nome = nomeTextField.text,
+              let calorias = caloriasTextField.text else {
+            return
+        }
+        
+        if let numeroDeCalorias = Double(calorias) {
+            let item = Item(nome: nome, calorias: numeroDeCalorias)
+            
+            navigationController?.popViewController(animated: true)
+        }
     }
 }
