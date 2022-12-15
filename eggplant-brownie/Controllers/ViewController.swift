1006a301167850acfea5bed38bd8d489854baf80
Natalia Braz <naabraz@gmail.com>
2022-12-15 09:44:26 -0300

    chore: add dao for itens

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -16,12 +16,7 @@ class ViewController: UIViewController,
     // MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
-    
-    var itens: [Item] = [Item(nome: "Molho de Tomate", calorias: 40.0),
-                         Item(nome: "Queijo", calorias: 40.0),
-                         Item(nome: "Molho Apimentado", calorias: 40.0),
-                         Item(nome: "Manjericão", calorias: 40.0)]
-    
+    var itens: [Item] = []
     var itensSelecionados: [Item] = []
     
     // MARK: - IBOutlets
@@ -38,17 +33,11 @@ class ViewController: UIViewController,
                                                 action: #selector(adicionarItens))
         navigationItem.rightBarButtonItem = botaoAdicionaItem
         
-        do {
-            guard let diretorio = recuperaDiretorio() else { return }
-            
-            let dados = try Data(contentsOf: diretorio)
-            
-            let itensSalvos = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(dados) as! [Item]
-            
-            itens = itensSalvos
-        } catch {
-            print(error.localizedDescription)
-        }
+        recuperaItens()
+    }
+    
+    func recuperaItens() {
+        itens = ItemDao().recupera()
     }
     
     @objc func adicionarItens() {
@@ -60,32 +49,13 @@ class ViewController: UIViewController,
     func add(_ item: Item) {
         itens.append(item)
         
-        itensTableView?.reloadData()
+        ItemDao().save(itens)
         
         if let tableView = itensTableView {
             tableView.reloadData()
         } else {
             Alerta(controller: self).exibe(titulo: "Desculpe", mensagem: "Não foi possível atualizar a tabela")
         }
-                
-        do {
-            let dados = try NSKeyedArchiver.archivedData(withRootObject: itens, requiringSecureCoding: false)
-            
-            guard let caminho = recuperaDiretorio() else { return }
-
-            try dados.write(to: caminho)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
-    func recuperaDiretorio() -> URL? {
-        guard let diretorio = FileManager.default.urls(for: .documentDirectory,
-                                                       in: .userDomainMask).first else { return nil }
-        
-        let caminho = diretorio.appendingPathComponent("itens")
-        
-        return caminho
     }
     
     // MARK: - UITableViewDataSource
---
+++ eggplant-brownie/Models/ItemDao.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+class ItemDao {
+
+    func recuperaDiretorio() -> URL? {
+        guard let diretorio = FileManager.default.urls(for: .documentDirectory,
+                                                       in: .userDomainMask).first else { return nil }
+        
+        let caminho = diretorio.appendingPathComponent("itens")
+        
+        return caminho
+    }
+    
+    func save(_ itens: [Item]) {
+        do {
+            let dados = try NSKeyedArchiver.archivedData(withRootObject: itens, requiringSecureCoding: false)
+            
+            guard let caminho = recuperaDiretorio() else { return }
+
+            try dados.write(to: caminho)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func recupera() -> [Item] {
+        do {
+            guard let diretorio = recuperaDiretorio() else { return [] }
+            
+            let dados = try Data(contentsOf: diretorio)
+            
+            let itensSalvos = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(dados) as! [Item]
+            
+            return itensSalvos
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
